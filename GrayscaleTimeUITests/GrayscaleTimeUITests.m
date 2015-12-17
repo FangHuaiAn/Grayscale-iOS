65f98ce9ed6f5f417478c8ccf3f6e85262ce984e
Fang Haui An <liddle.fang@gmail.com>
2015-12-17 16:29:24 +0800

    update

--- GrayscaleTimeUITests/GrayscaleTimeUITests.m
+++ GrayscaleTimeUITests/GrayscaleTimeUITests.m
@@ -17,6 +17,7 @@
 - (void)setUp {
     [super setUp];
     
+    
     // Put setup code here. This method is called before the invocation of each test method in the class.
     
     // In UI tests it is usually best to stop immediately when a failure occurs.
@@ -35,6 +36,40 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    [XCUIDevice sharedDevice].orientation = UIDeviceOrientationFaceUp;
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *grayscaleButton = app.buttons[@"Grayscale"];
+    [grayscaleButton tap];
+    [app.buttons[@"Reload"] tap];
+    [grayscaleButton tap];
+    
+    
+    
 }
+
+- (void) testRealDevice {
+
+    [XCUIDevice sharedDevice].orientation = UIDeviceOrientationFaceUp;
+    [XCUIDevice sharedDevice].orientation = UIDeviceOrientationFaceUp;
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *grayscaleButton = app.buttons[@"Grayscale"];
+    [grayscaleButton tap];
+    [app.buttons[@"Reload"] tap];
+    [grayscaleButton tap];
+    
+    // data source
+    
+    
+    [grayscaleButton tap];
+    [grayscaleButton tap];
+    [app.buttons[@"Reload"] tap];
+    
+    
+
+
+}
+
 
 @end
